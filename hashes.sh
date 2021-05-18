b059a24ffd02215f686933ddf69846174ab66442
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2021-05-18 11:41:17 -0700

    try

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=0bc8c41207a36fb78240eea47bad1f72031f52cf
+hashes[FStar]=825e7cda53e0b06345a965b9d316d65040a33cac
 branches[FStar]=master
-hashes[hacl-star]=04a5132a3873fb92cd9f1d9ba21739a3b0502373
-branches[hacl-star]=master
-hashes[kremlin]=5a8c41548147e548b258cefa755064a48cb31b39
-branches[kremlin]=master
-hashes[mitls-fstar]=6b6bc681078dbfac1d2e61cfd0199773c9bfc608
+hashes[hacl-star]=c08c2dd173539ee66376e5365d1ecba7a2ceaafa
+branches[hacl-star]=protz_election_guard
+hashes[kremlin]=89b38b0394f777c14479d51c47eb5dab251ee309
+branches[kremlin]=protz_election_guard
+hashes[mitls-fstar]=436de7522cc55d05e4481d44d173bda2b80a4501
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=e18fc514da6d7ecf23b4b2419804e20081225493
+hashes[quackyducky]=538bda1a6d1320ea6ca145971a2f7875ac9b49a1
 branches[quackyducky]=master
